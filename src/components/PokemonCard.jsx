191388848bf19ef6ef12f67111839dc7772d4bb8
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-10-19 10:31:12 -0300

    integrating PokeApi to the project

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,9 +3,11 @@ import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import './PokemonList.css';
 
-const PokemonCard = () => {
+const PokemonCard = ({ name }) => {
   return (
-    <Card title="Charizard" cover={<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png" alt="Charizard" />}
+    <Card 
+    title={ name } 
+    cover={<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png" alt="Charizard" />}
         extra={<StarOutlined />}
     >
       <Meta description="fire, flying" />
