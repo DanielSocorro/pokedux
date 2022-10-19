a2f42b6db6a31ce9a15297a1afdfd182821c9cdb
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-10-19 18:40:09 -0300

    Hooks vs. Connect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { pokemonReducer } from './reducers/pokemons';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = createStore(pokemonReducer);
+
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
